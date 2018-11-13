35a381827b10bd9550e3be7d0555361c2762363f
Kaveesha Gimhana <36946467+KaveeshaG@users.noreply.github.com>
2018-11-14 01:53:16 +0530

    OOP files
    basic oop programs

---
+++ Earth.java
@@ -0,0 +1,19 @@
+
+public class Earth {
+	public static void main(String args[]) {
+		
+		Test john;
+		john = new Test();
+		
+		john.age = 18;
+		john.name = "johny";
+		john.eyecolour = "red";
+		john.HeightInch = 80;
+		
+		john.eat();
+		john.sleep();
+		john.walk();
+		john.speak();
+		
+	}
+}
---
+++ Test.java
@@ -0,0 +1,32 @@
+
+public class Test {
+	int age;
+	String name;
+	int HeightInch;
+	String eyecolour;
+	
+	public Test(){
+		
+	}
+	
+	public void speak() {
+		System.out.println("I am "+age+" yers old.");
+		System.out.println("My Name is "+name);
+		System.out.println("My height is "+HeightInch);
+		System.out.println("Eye colour is "+eyecolour);
+	}
+	
+	public void walk() {
+		System.out.println("Walking....");
+	}
+	
+	public void eat() {
+		System.out.println("Eating....");
+	}
+	
+	public void sleep() {
+		System.out.println("Sleeping....");
+	}
+	
+
+}
